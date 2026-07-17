50a29c8d9674a26c820facafb5b934323ec5b6bd
rjmlaird <rjmlaird@gmail.com>
2026-07-17 18:59:14 +0100

    more tools

--- src/data/tools.ts
+++ src/data/tools.ts
@@ -8,10 +8,12 @@ export const tools = {
         { "name": "JavaScript", "url": "https://developer.mozilla.org/en-US/docs/Web/JavaScript", "logo": "javascript", "color": "F7DF1E", "logoColor": "black" },
         { "name": "TypeScript", "url": "https://www.typescriptlang.org/", "logo": "typescript", "color": "3178C6" },
         { "name": "Python", "url": "https://www.python.org/", "logo": "python", "color": "3776AB" },
+        { "name": "R", "url": "https://www.r-project.org/", "logo": "r", "color": "276DC3" },
         { "name": "SQL", "url": "https://en.wikipedia.org/wiki/SQL", "logo": "postgresql", "color": "4169E1" },
         { "name": "C", "url": "https://en.cppreference.com/w/c", "logo": "c", "color": "A8B9CC" },
         { "name": "C++", "url": "https://isocpp.org/", "logo": "cplusplus", "color": "00599C" },
-        { "name": "IDL", "url": "https://www.nv5geospatialsoftware.com/Products/IDL", "logo": "code", "color": "007396" }
+        { "name": "IDL", "url": "https://www.nv5geospatialsoftware.com/Products/IDL", "logo": "code", "color": "007396" },
+        { "name": "LaTeX", "url": "https://www.latex-project.org/", "logo": "latex", "color": "008080" }
       ]
     },
     {
@@ -48,39 +50,43 @@ export const tools = {
       ]
     },
     {
-      "name": "Applied AI, Data & LLM Workflows",
+      "name": "Applied AI, Data & Research",
       "items": [
         { "name": "OpenAI", "url": "https://platform.openai.com/", "logo": "openai", "color": "412991" },
         { "name": "Hugging Face", "url": "https://huggingface.co/", "logo": "huggingface", "color": "FF9D00" },
         { "name": "LangChain", "url": "https://www.langchain.com/", "logo": "langchain", "color": "000000" },
         { "name": "Jupyter", "url": "https://jupyter.org/", "logo": "jupyter", "color": "F37626" },
+        { "name": "Tableau", "url": "https://www.tableau.com/", "logo": "tableau", "color": "E97627" },
+        { "name": "QGIS", "url": "https://qgis.org/", "logo": "qgis", "color": "589632" },
         { "name": "Google Earth Engine", "url": "https://earthengine.google.com/", "logo": "googleearth", "color": "4285F4" },
         { "name": "Metagraph", "url": "https://metagraph.dev/", "logo": "activity", "color": "7B61FF" }
       ]
     },
     {
-      "name": "Marketing & Ops Tools",
+      "name": "Marketing, SEO & Analytics",
       "items": [
         { "name": "HubSpot", "url": "https://www.hubspot.com/", "logo": "hubspot", "color": "FF7A59" },
+        { "name": "Ahrefs", "url": "https://ahrefs.com/", "logo": "search", "color": "0085FF" },
+        { "name": "Plausible", "url": "https://plausible.io/", "logo": "chart", "color": "5850EC" },
+        { "name": "Matomo", "url": "https://matomo.org/", "logo": "matomo", "color": "D32F2F" },
         { "name": "MailerLite", "url": "https://www.mailerlite.com/", "logo": "mail", "color": "3B82F6" },
         { "name": "SendGrid", "url": "https://sendgrid.com/", "logo": "sendgrid", "color": "00A8E8" },
         { "name": "EcoSend", "url": "https://ecosend.io/", "logo": "leaf", "color": "2D8A5E" },
         { "name": "Mailchimp", "url": "https://mailchimp.com/", "logo": "mailchimp", "color": "FFE01B" },
-        { "name": "Slack", "url": "https://slack.com/", "logo": "slack", "color": "4A154B" },
-        { "name": "Notion", "url": "https://www.notion.so/", "logo": "notion", "color": "000000" },
         { "name": "Screaming Frog", "url": "https://www.screamingfrog.co.uk/", "logo": "frog", "color": "D1232A" }
       ]
     },
     {
-      "name": "Project & Finance Management",
+      "name": "Ops, Finance & Knowledge",
       "items": [
+        { "name": "Slack", "url": "https://slack.com/", "logo": "slack", "color": "4A154B" },
+        { "name": "Notion", "url": "https://www.notion.so/", "logo": "notion", "color": "000000" },
+        { "name": "Obsidian", "url": "https://obsidian.md/", "logo": "obsidian", "color": "7B38D8" },
+        { "name": "Overleaf", "url": "https://www.overleaf.com/", "logo": "latex", "color": "47A141" },
         { "name": "Asana", "url": "https://asana.com/", "logo": "asana", "color": "FC6060" },
-        { "name": "Trello", "url": "https://trello.com/", "logo": "trello", "color": "0079BF" },
-        { "name": "Jira", "url": "https://www.atlassian.com/software/jira", "logo": "jira", "color": "0052CC" },
         { "name": "Linear", "url": "https://linear.app/", "logo": "linear", "color": "5E6AD2" },
         { "name": "Stripe", "url": "https://stripe.com/", "logo": "stripe", "color": "635BFF" },
-        { "name": "Xero", "url": "https://www.xero.com/", "logo": "xero", "color": "13B5EA" },
-        { "name": "Namecheap", "url": "https://www.namecheap.com/", "logo": "domain", "color": "DE3163" }
+        { "name": "Xero", "url": "https://www.xero.com/", "logo": "xero", "color": "13B5EA" }
       ]
     },
     {
@@ -99,9 +105,9 @@ export const tools = {
         { "name": "GitHub", "url": "https://github.com/", "logo": "github", "color": "181717" },
         { "name": "VS Code", "url": "https://code.visualstudio.com/", "logo": "visualstudiocode", "color": "007ACC" },
         { "name": "Cursor", "url": "https://cursor.com/", "logo": "cursor", "color": "000000" },
+        { "name": "Postman", "url": "https://www.postman.com/", "logo": "postman", "color": "FF6C37" },
         { "name": "npm", "url": "https://www.npmjs.com/", "logo": "npm", "color": "CB3837" },
-        { "name": "pnpm", "url": "https://pnpm.io/", "logo": "pnpm", "color": "F9AD00" },
-        { "name": "Stremio", "url": "https://www.stremio.com/", "logo": "play", "color": "7367F0" }
+        { "name": "pnpm", "url": "https://pnpm.io/", "logo": "pnpm", "color": "F9AD00" }
       ]
     }
   ]
